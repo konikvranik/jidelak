dfd9b9f0a0624e0dd06a611aeea4a6e475d7a78b
Petr <petr@vranik.name>
2014-01-05 00:59:31 +0100

    templaty

--- jidelak/src/main/java/net/suteren/android/jidelak/JidelakFeederService.java
+++ jidelak/src/main/java/net/suteren/android/jidelak/JidelakFeederService.java
@@ -1,5 +1,25 @@
 package net.suteren.android.jidelak;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.tidy.Tidy;
+
 import android.app.Service;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -27,5 +47,38 @@ public class JidelakFeederService extends Service {
 
 		registerReceiver(new JidelakFeederReceiver(), new IntentFilter(
 				Intent.ACTION_TIME_TICK));
+	}
+
+	public Node retrieve(URL url, InputStream inXsl) throws IOException,
+			TransformerException, ParserConfigurationException {
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		Document d = tidy(con.getInputStream(), con.getContentEncoding());
+		DOMResult res = transform(d, inXsl);
+		return res.getNode();
+	}
+
+	private Document tidy(InputStream is, String enc) throws IOException {
+		Tidy t = new Tidy();
+		t.setInputEncoding(enc == null ? "cp1250" : enc);
+		t.setNumEntities(true);
+		t.setXmlOut(true);
+		t.setShowWarnings(false);
+		t.setTrimEmptyElements(true);
+		// t.setQuoteNbsp(true);
+		Document d = t.parseDOM(is, null);
+		is.close();
+		return d;
+	}
+
+	private DOMResult transform(Document d, InputStream inXsl)
+			throws IOException, TransformerConfigurationException,
+			TransformerFactoryConfigurationError, ParserConfigurationException,
+			TransformerException {
+		Transformer tr = TransformerFactory.newInstance().newTransformer(
+				new StreamSource(inXsl));
+		DOMResult res = new DOMResult(DocumentBuilderFactory.newInstance()
+				.newDocumentBuilder().newDocument());
+		tr.transform(new DOMSource(d), res);
+		return res;
 	}
 }

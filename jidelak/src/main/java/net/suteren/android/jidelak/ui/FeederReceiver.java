65abc1be8e6bb0a1fcd3dd0766d0d92aaf8e6a17
jd39426 <jd39426@W2A0018V.TDA001.AD.ACC.SYS>
2014-02-03 15:22:41 +0100

    verze 53

--- jidelak/src/main/java/net/suteren/android/jidelak/ui/FeederReceiver.java
+++ jidelak/src/main/java/net/suteren/android/jidelak/ui/FeederReceiver.java
@@ -24,8 +24,7 @@ import android.content.SharedPreferences;
  */
 public class FeederReceiver extends BroadcastReceiver {
 
-	private static Logger log = LoggerFactory
-			.getLogger(FeederReceiver.class);
+	private static Logger log = LoggerFactory.getLogger(FeederReceiver.class);
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
@@ -36,17 +35,22 @@ public class FeederReceiver extends BroadcastReceiver {
 		} else if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0) {
 			log.debug("DemoReceiver.onReceive(ACTION_TIME_TICK)");
 
-			if (decideIfStart(context))
-				context.startService(new Intent(context,
-						FeederService.class));
+			if (decideIfStart(context)) {
+				log.debug("Receiver is starting service...");
+				context.startService(new Intent(context, FeederService.class));
+			}
 		} else
 			log.debug("DemoReceiver.onReceive(" + intent.getAction() + ")");
 	}
 
 	private boolean decideIfStart(Context context) {
 
+		log.debug("deciding if start");
+
 		if (!NetworkUtils.isConnected(context))
 			return false;
+
+		log.debug("deciding if start: network ok");
 
 		SharedPreferences prefs = context.getSharedPreferences(
 				DEFAULT_PREFERENCES, Context.MODE_PRIVATE);
@@ -57,6 +61,8 @@ public class FeederReceiver extends BroadcastReceiver {
 				&& NetworkUtils.isConnectedMobile(context))
 			return false;
 
+		log.debug("deciding if start: wifi ok");
+
 		try {
 			if (schedule != -1)
 				schedule += prefs.getLong(UPDATE_INTERVAL_KEY,
@@ -64,6 +70,8 @@ public class FeederReceiver extends BroadcastReceiver {
 		} catch (ClassCastException e) {
 			prefs.edit().putLong(UPDATE_INTERVAL_KEY, DEFAULT_UPDATE_INTERVAL);
 		}
+
+		log.debug("deciding if start: " + time + ">" + schedule);
 
 		if (time > schedule) {
 			return true;

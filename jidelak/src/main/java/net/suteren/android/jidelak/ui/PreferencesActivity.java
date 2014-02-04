a10795fa1e3931983893a70ea6920a28370a63a5
Petr <petr@vranik.name>
2014-02-04 21:52:47 +0100

    actionbar in preferenceactivity

--- jidelak/src/main/java/net/suteren/android/jidelak/ui/PreferencesActivity.java
+++ jidelak/src/main/java/net/suteren/android/jidelak/ui/PreferencesActivity.java
@@ -5,6 +5,9 @@ package net.suteren.android.jidelak.ui;
 
 import static net.suteren.android.jidelak.Constants.DEFAULT_PREFERENCES;
 import net.suteren.android.jidelak.R;
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
@@ -20,10 +23,21 @@ public class PreferencesActivity extends PreferenceActivity implements
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+			setupActionBar();
 
 		getPreferenceManager().setSharedPreferencesName(DEFAULT_PREFERENCES);
 		addPreferencesFromResource(R.xml.main_prefs);
 
+	}
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private ActionBar setupActionBar() {
+		ActionBar ab = getActionBar();
+		ab.setDisplayHomeAsUpEnabled(true);
+		ab.setDisplayShowHomeEnabled(true);
+		ab.setDisplayShowTitleEnabled(false);
+		return ab;
 	}
 
 	@Override

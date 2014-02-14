ff7a0385c4c21f0bb0b90b2c053a02a709a4b5df
jd39426 <jd39426@W2A0018V.TDA001.AD.ACC.SYS>
2014-02-14 10:28:00 +0100

    podpora map

---
+++ jidelak/src/main/java/net/suteren/android/jidelak/MapActivity.java
@@ -0,0 +1,17 @@
+package net.suteren.android.jidelak;
+
+import net.suteren.android.jidelak.ui.AbstractJidelakActivity;
+import android.os.Bundle;
+
+public class MapActivity extends AbstractJidelakActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.map_layout);
+
+		GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(
+				R.id.map)).getMap();
+	}
+
+}

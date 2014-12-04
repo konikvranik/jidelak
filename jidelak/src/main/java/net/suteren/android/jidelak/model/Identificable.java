da146324f4a94b2ed18e992e429c63d6cf157237
hPa <petr@vranik.name>
2014-12-04 11:15:13 +0100

    cleanup

--- jidelak/src/main/java/net/suteren/android/jidelak/model/Identificable.java
+++
@@ -1,9 +0,0 @@
-package net.suteren.android.jidelak.model;
-
-import java.io.Serializable;
-
-public interface Identificable<T> extends Comparable<T>, Serializable {
-	Long getId();
-
-	void setId(Long id);
-}
--- jidelak/src/main/java/net/suteren/android/jidelak/model/TimeType.java
+++
@@ -1,5 +0,0 @@
-package net.suteren.android.jidelak.model;
-
-public enum TimeType {
-	ABSOLUTE, RELATIVE;
-}

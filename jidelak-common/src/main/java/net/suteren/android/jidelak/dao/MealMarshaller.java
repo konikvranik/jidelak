936993597d8b8d0773f9078679df3cc597f12375
Petr Vranik <petr.vranik@lmc.eu>
2016-04-14 16:48:00 +0200

    more debug info

--- jidelak-common/src/main/java/net/suteren/android/jidelak/dao/MealMarshaller.java
+++ jidelak-common/src/main/java/net/suteren/android/jidelak/dao/MealMarshaller.java
@@ -16,120 +16,122 @@ import java.util.Map;
 
 public class MealMarshaller extends BaseMarshaller<Meal> {
 
-	private Source source;
-	private static Logger log = LoggerFactory.getLogger(MealMarshaller.class);
+    private Source source;
+    private static Logger log = LoggerFactory.getLogger(MealMarshaller.class);
 
-	@Override
-	protected void unmarshallHelper(String prefix, Map<String, String> data,
-			Meal meal) throws JidelakException {
+    @Override
+    protected void unmarshallHelper(String prefix, Map<String, String> data,
+                                    Meal meal) throws JidelakException {
 
-		meal.setTitle(data.get(prefix + "meal.title"));
-		meal.setDescription(data.get(prefix + "meal.description"));
-		meal.setPrice(data.get(prefix + "meal.price"));
-		meal.setCategory(data.get(prefix + "meal@category"));
-		String dishString = data.get(prefix + "meal@dish");
-		log.debug("Data: %s", data.toString());
-		log.debug("Meal: %s", meal);
-		try {
-			if (dishString != null && !"".equals(dishString.trim()))
-				meal.setDish(Dish.valueOf(dishString.trim().toUpperCase(
-						Locale.ENGLISH)));
+        meal.setTitle(data.get(prefix + "meal.title"));
+        meal.setDescription(data.get(prefix + "meal.description"));
+        meal.setPrice(data.get(prefix + "meal.price"));
+        meal.setCategory(data.get(prefix + "meal@category"));
+        String dishString = data.get(prefix + "meal@dish");
+        log.debug("Data: %s", data.toString());
+        log.debug("Meal: %s", meal);
+        try {
+            if (dishString != null && !"".equals(dishString.trim())) {
+                meal.setDish(Dish.valueOf(dishString.trim().toUpperCase(
+                        Locale.ENGLISH)));
+                log.debug("Dish set to: " + meal.getDish().name());
+            } else {
+                log.debug("Dish is %", meal.getDish() == null ? "null" : "empty");
+            }
+        } catch (IllegalArgumentException e) {
+            throw new JidelakException("string.invalid_dish", e)
+                    .setMeal(meal)
+                    .setRestaurant(
+                            meal.getSource() == null ? meal.getSource()
+                                    .getRestaurant() : meal.getRestaurant())
+                    .setSource(meal.getSource())
+                    .setArgs(new String[]{dishString}).setHandled(true);
+        }
+        String o = data.get(prefix + "meal@order");
+        if (o != null)
+            meal.setPosition(Integer.parseInt(o));
 
-			log.debug("Dish set to: " + meal.getDish().name());
-		} catch (IllegalArgumentException e) {
-			throw new JidelakException("string.invalid_dish", e)
-					.setMeal(meal)
-					.setRestaurant(
-							meal.getSource() == null ? meal.getSource()
-									.getRestaurant() : meal.getRestaurant())
-					.setSource(meal.getSource())
-					.setArgs(new String[] { dishString }).setHandled(true);
-		}
-		String o = data.get(prefix + "meal@order");
-		if (o != null)
-			meal.setPosition(Integer.parseInt(o));
+        String x = data.get(prefix + "meal@time");
+        Calendar cal = Calendar.getInstance(getSource().getLocale());
+        cal.setTimeInMillis(System.currentTimeMillis());
+        switch (getSource().getTimeType()) {
+            case RELATIVE:
 
-		String x = data.get(prefix + "meal@time");
-		Calendar cal = Calendar.getInstance(getSource().getLocale());
-		cal.setTimeInMillis(System.currentTimeMillis());
-		switch (getSource().getTimeType()) {
-		case RELATIVE:
+                if (x != null) {
 
-			if (x != null) {
+                    String y = data.get(prefix + "meal@ref-time");
+                    try {
 
-				String y = data.get(prefix + "meal@ref-time");
-				try {
+                        cal.setTime(getSource().getDateFormat().parse(y));
+                        cal.add(getSource().getOffsetBase().getType(), getSource()
+                                .getOffset());
+                    } catch (ParseException e) {
+                        log.warn(e.getMessage(), e);
+                        throw new JidelakParseException(
+                                "string.meal_invalid_date_format", getSource()
+                                .getDateFormatString(), x, e)
+                                .setMeal(meal)
+                                .setRestaurant(
+                                        meal.getSource() == null ? meal
+                                                .getRestaurant() : meal.getSource()
+                                                .getRestaurant())
+                                .setSource(meal.getSource()).setHandled(true);
+                    }
+                    try {
+                        cal.add(Calendar.DAY_OF_MONTH, Integer.parseInt(x.trim()));
 
-					cal.setTime(getSource().getDateFormat().parse(y));
-					cal.add(getSource().getOffsetBase().getType(), getSource()
-							.getOffset());
-				} catch (ParseException e) {
-					log.warn(e.getMessage(), e);
-					throw new JidelakParseException(
-							"string.meal_invalid_date_format", getSource()
-									.getDateFormatString(), x, e)
-							.setMeal(meal)
-							.setRestaurant(
-									meal.getSource() == null ? meal
-											.getRestaurant() : meal.getSource()
-											.getRestaurant())
-							.setSource(meal.getSource()).setHandled(true);
-				}
-				try {
-					cal.add(Calendar.DAY_OF_MONTH, Integer.parseInt(x.trim()));
+                    } catch (NumberFormatException e) {
+                        log.warn(e.getMessage(), e);
+                        throw new JidelakException(
+                                "string.meal_invalid_integer_format", e, x)
+                                .setMeal(meal)
+                                .setRestaurant(
+                                        meal.getSource() == null ? meal
+                                                .getRestaurant() : meal.getSource()
+                                                .getRestaurant())
+                                .setSource(meal.getSource()).setHandled(true);
+                    }
+                }
+                break;
 
-				} catch (NumberFormatException e) {
-					log.warn(e.getMessage(), e);
-					throw new JidelakException(
-							"string.meal_invalid_integer_format", e, x)
-							.setMeal(meal)
-							.setRestaurant(
-									meal.getSource() == null ? meal
-											.getRestaurant() : meal.getSource()
-											.getRestaurant())
-							.setSource(meal.getSource()).setHandled(true);
-				}
-			}
-			break;
+            case ABSOLUTE:
+                try {
 
-		case ABSOLUTE:
-			try {
+                    log.debug("Parsing " + x + " by "
+                            + getSource().getDateFormatString());
+                    if (x != null)
+                        cal.setTime(getSource().getDateFormat().parse(x.trim()));
+                    if (cal.get(Calendar.YEAR) == 1970)
+                        cal.set(Calendar.YEAR,
+                                Calendar.getInstance().get(Calendar.YEAR));
+                } catch (ParseException e) {
+                    log.warn(e.getMessage() + " at " + e.getErrorOffset(), e);
+                    throw new JidelakParseException(
+                            "string.meal_invalid_date_format", getSource()
+                            .getDateFormatString(), x, e)
+                            .setMeal(meal)
+                            .setRestaurant(
+                                    meal.getSource() == null ? meal.getRestaurant()
+                                            : meal.getSource().getRestaurant())
+                            .setSource(meal.getSource()).setHandled(true);
+                }
+                break;
 
-				log.debug("Parsing " + x + " by "
-						+ getSource().getDateFormatString());
-				if (x != null)
-					cal.setTime(getSource().getDateFormat().parse(x.trim()));
-				if (cal.get(Calendar.YEAR) == 1970)
-					cal.set(Calendar.YEAR,
-							Calendar.getInstance().get(Calendar.YEAR));
-			} catch (ParseException e) {
-				log.warn(e.getMessage() + " at " + e.getErrorOffset(), e);
-				throw new JidelakParseException(
-						"string.meal_invalid_date_format", getSource()
-								.getDateFormatString(), x, e)
-						.setMeal(meal)
-						.setRestaurant(
-								meal.getSource() == null ? meal.getRestaurant()
-										: meal.getSource().getRestaurant())
-						.setSource(meal.getSource()).setHandled(true);
-			}
-			break;
+            default:
+                break;
+        }
 
-		default:
-			break;
-		}
+        meal.setAvailability(new Availability(cal));
 
-		meal.setAvailability(new Availability(cal));
+    }
 
-	}
+    public void setSource(Source source) {
+        this.source = source;
+    }
 
-	public void setSource(Source source) {
-		this.source = source;
-	}
-
-	public Source getSource() {
-		if (source == null)
-			return new Source();
-		return source;
-	}
+    public Source getSource() {
+        if (source == null)
+            return new Source();
+        return source;
+    }
 }
